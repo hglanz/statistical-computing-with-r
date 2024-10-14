63a4a42f36eaf18a81b13d99ca67c2d1af0910be
Allison Theobold <atheobol@calpoly.edu>
2024-10-14 14:50:13 -0700

    clean up process of making groups for PA, remove files from being rendered

--- group-activities/group-formation/group-formation.R
+++ group-activities/group-formation/group-formation.R
@@ -4,53 +4,67 @@ library(readxl)
 
 set.seed(1234)
 
+# Read in consent forms
 consent_forms <- read_csv(here::here("group-activities", 
                                      "group-formation", 
                                      "consent-form.csv")
                           ) |> 
   rename(full_name = `Please type your full name:`, 
          consent = Consent, 
-         email = `Email Address`)
+         email = `Email Address`) |> 
+  mutate(consent = if_else(consent == "Yes, I agree to participate.", 
+                           "Yes", 
+                           "No")
+         )
 
-weeks2_3 <- read_csv(here::here("group-activities", 
-                                "group-formation", 
-                                "Weeks 2 & 3 Practice Activity Groups.csv")
-                     )
+canvas_groups_week3 <- read_csv(here::here("group-activities", 
+                                           "group-formation", 
+                                           "canvas-group-template.csv")
+                                )
 
+## Form groups by shuffling within the Y / N consent groups
+weeks_3_groups <- sample(consent_forms, 
+                         replace = FALSE, 
+                         groups = consent_forms$consent, 
+                         orig.ids = FALSE) |> 
+  mutate(group_name = c(
+    rep("Group 1", 2),
+    rep("Group 2", 2),
+    rep("Group 3", 2),
+    rep("Group 4", 2),
+    rep("Group 5", 2),
+    rep("Group 6", 2),
+    rep("Group 7", 2),
+    rep("Group 8", 2),
+    rep("Group 9", 2),
+    rep("Group 10", 2),
+    rep("Group 11", 2),
+    rep("Group 12", 2),
+    rep("Group 13", 2),
+    rep("Group 14", 2),
+    rep("Group 15", 2),
+    rep("Group 16", 2),
+    rep("Group 17", 2),
+    rep("Group 18", 2)
+    )
+    ) |> 
+  # select only columns to join on
+  select(email, 
+         group_name)
 
-yes <- filter(consent_forms,
-              consent == "Yes, I agree to participate.") |> 
-  pull(email)
+final_canvas_groups_wk3 <- left_join(canvas_groups_week3, 
+                                     weeks_3_groups, 
+                                     by = join_by(login_id == email)
+                                     ) |> 
+  select(-group_name.x) |>
+  rename(group_name = group_name.y)
 
-yes_groups <- replicate(n = 4, 
-                        sample(yes, size = 2, replace = FALSE)
-) |> 
-  as.data.frame() |> 
-  rename(`Group 1` = V1, 
-         `Group 2` = V2,
-         `Group 3` = V3,
-         `Group 4` = V4)
+write_csv(final_canvas_groups_wk3, 
+          file = here::here("group-activities", 
+                            "group-formation", 
+                            "Week 4 Practice Activity Groups.csv")
+          )  
 
-no <- filter(consent_forms,
-              consent == "No, I do not agree to participate.") |> 
-  pull(email)
-
-no_groups <- replicate(n = 12, 
-                       sample(no, size = 2, replace = FALSE)
-                       ) |> 
-  as.data.frame() |> 
-  rename(`Group 5` = V1,
-         `Group 6` = V2,
-         `Group 7` = V3,
-         `Group 8` = V4,
-         `Group 9` = V5,
-         `Group 10` = V6,
-         `Group 11` = V7,
-         `Group 12` = V8,
-         `Group 13` = V9,
-         `Group 14` = V10,
-         `Group 15` = V11,
-         `Group 16` = V12)
 
 
 
