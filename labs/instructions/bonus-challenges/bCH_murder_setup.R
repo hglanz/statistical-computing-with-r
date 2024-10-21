6e1c5d2929ba9b8857e194cea8c37b2969d9b8fc
Allison Theobold <atheobol@calpoly.edu>
2024-10-21 09:56:27 -0500

    student / instructions for lab 5 murder mystery

--- labs/instructions/bonus-challenges/bCH_murder_setup.R
+++ labs/instructions/bonus-challenges/bCH_murder_setup.R
@@ -1,11 +1,24 @@
-table_names <- c("crime_scene_report","drivers_license",
+table_names <- c("crime_scene_report",
+                 "drivers_license",
                  "facebook_event_checkin",
-                 "get_fit_now_check_in","get_fit_now_member",
-                 "income", "interview","person")
+                 "get_fit_now_check_in",
+                 "get_fit_now_member",
+                 "income", 
+                 "interview",
+                 "person")
 
 url_base <- "https://raw.githubusercontent.com/zoerehnberg/STAT331-S23/main/challenges/bCH_murder_data/"
 
-# For each name, read in the table and store it as the name.
-purrr::walk(table_names, function(x) {
-  assign(x, readr::read_csv(paste0(url_base, x, ".csv")), envir = .GlobalEnv)
-})
+# Function to paste the URL with the name of the table, 
+# Store the resulting table in the Global Environment
+read_in_tables <- function(x){ 
+  assign(x, 
+         value = readr::read_csv(
+           str_flatten(url_base, x, ".csv")
+           ), 
+         envir = .GlobalEnv)
+}
+
+# For each name in the table names, read in the table and store it as the name.
+purrr::walk(.x = table_names, 
+            .f = read_in_tables)
